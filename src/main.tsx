2a7ff92865d1ba6c9c116bcea9ec1b9ae7b135c2
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-07-22 16:10:20 +0000

    [Enh]: Init simple apollo client with test api.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+const TESTING_URI = "https://flyby-router-demo.herokuapp.com/";
+
+const client = new ApolloClient({
+  uri: TESTING_URI,
+  cache: new InMemoryCache(),
+});
+
+// Changed to useQuery.
+/* client
+  .query({
+    query: GET_LOCATIONS,
+  })
+  .then((result) => console.log(result))
+  .catch(console.error); */
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-)
+);
